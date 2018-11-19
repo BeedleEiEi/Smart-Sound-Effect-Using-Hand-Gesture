9fa53ffb2bd6fcdbfcebd4b91f0cf90a1c348ac8
smarty0123 <smarty_0123@hotmail.com>
2018-11-19 18:15:35 +0700

    init handgesture like an example

--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,15 @@
 
 #include <QMainWindow>
 
+#include <QTimer>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace cv;
+
+#include <iostream>
+using namespace std;
+
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +24,23 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_openCamButton_clicked();
+
+    void process();
+
+    QImage convertOpenCVMatToQtQImage(cv::Mat mat);
+
+    void createTrackbars();
+
 private:
     Ui::MainWindow *ui;
+
+    QTimer *timer;
+    VideoCapture cap;
+
+    Mat frame;
+    QImage qt_image;
 };
 
 #endif // MAINWINDOW_H
